53a1cd9caba45d86f0356662bc5b108e31012f13
Octavian <octavian@Christines-MacBook-Air.local>
2021-02-11 23:52:17 +0300

    Displaying data

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,23 +1,10 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "Feb 6th at 1:30pm",
-    reminder: true,
-  },
-];
+import Task from "./Task";
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
   return (
     <div>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </div>
   );
